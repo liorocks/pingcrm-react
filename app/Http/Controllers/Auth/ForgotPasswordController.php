db40fee7d5e6a57fe8a6b86b85823a088a418fcc
Matthieu MARTIN <codingstuff91@gmail.com>
2022-11-11 16:43:01 +0100

    Add ForgotPassword view and edit ForgotPasswordController

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Mail\ForgotPasswordMail;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
@@ -28,5 +33,22 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function forgotPasswordPage()
+    {
+        return Inertia::render('Auth/ForgotPassword');
+    }
+
+    public function requestNewPassword(Request $request)
+    {
+        $user = User::where('email', $request->email)->get()->first();
+
+        return !$user ? "Oops. Who are you ?" : $this->sendResetPasswordEmail($user->email);
+    }
+
+    private function sendResetPasswordEmail($email)
+    {
+        Mail::to($email)->send(new ForgotPasswordMail());
     }
 }
