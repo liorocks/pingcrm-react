5453e0db59d887da76280a7736b74317a72b50eb
Matthieu MARTIN <codingstuff91@gmail.com>
2022-11-11 16:44:01 +0100

    Create ForgotPassword Mail

---
+++ app/Mail/ForgotPasswordMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ForgotPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('exam@onepilot', 'Example')->markdown('emails.forgot_password');
+    }
+}
---
+++ resources/views/emails/forgot_password.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Hello One Pilot
+
+The answer is 42
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
