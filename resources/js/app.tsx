1864f2f6a87de03b029155e9c2fc6dedc83af3b7
Lio <liodotdev@gmail.com>
2024-05-21 14:26:18 +0200

    wip

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,8 +8,8 @@ createInertiaApp({
   title: title => `${title} - ${appName}`,
   resolve: name =>
     resolvePageComponent(
-      `./Pages/${name}.tsx`,
-      import.meta.glob('./Pages/**/*.tsx')
+      `./pages/${name}.tsx`,
+      import.meta.glob('./pages/**/*.tsx')
     ),
   setup({ el, App, props }) {
     const root = createRoot(el);
